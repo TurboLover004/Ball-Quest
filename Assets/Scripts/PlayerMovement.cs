df2073c3064787c5e3558b1c4f18ee5662760e69
Steve France <stephenfrance004@gmail.com>
2017-05-20 20:52:00 +0100

    First booster pad prototype

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -3,14 +3,23 @@ using System.Collections;
 
 public class PlayerMovement : MonoBehaviour
 {
+    private Renderer playerRenderer;
     private Rigidbody playerBody;
 
-    public float speed;
+    public float acceleration;
 
 	void Start ()
     {
+        playerRenderer = GetComponent<Renderer>();
         playerBody = GetComponent<Rigidbody>();
 	}
+
+    void Update()
+    {
+        float speed = playerBody.GetPointVelocity(playerBody.transform.position).magnitude;
+
+        playerRenderer.material.color = new Color(1, Mathf.Clamp(1 - (speed / 15), 0f, 1f), 0, 1);
+    }
 
     void FixedUpdate()
     {
@@ -18,11 +27,24 @@ public class PlayerMovement : MonoBehaviour
         {
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             Vector3 movement = new Vector3(ray.direction.x * 10, 0.0f, ray.direction.z * 10);
-			
-			float movementLength = Mathf.Sqrt(Mathf.Pow(movement.x, 2) + Mathf.Pow(movement.z, 2));
-			movement = speed * (movement/movementLength);
 
-            playerBody.AddForce(movement * speed);
+            playerBody.AddForce((movement / movement.magnitude) * acceleration);
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Booster"))
+        {
+            acceleration = acceleration * 3;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Booster"))
+        {
+            acceleration = acceleration / 3;
         }
     }
 }
