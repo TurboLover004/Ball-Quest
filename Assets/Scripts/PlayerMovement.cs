5c782aec9e30169849b403d3da024f906c7fd3e7
Steve France <stephenfrance004@gmail.com>
2017-05-20 03:32:49 +0100

    First player movement prototype

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Rigidbody playerBody;
+
+    public float speed;
+
+	void Start ()
+    {
+        playerBody = GetComponent<Rigidbody>();
+	}
+
+    void FixedUpdate()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            Vector3 movement = new Vector3(ray.direction.x * 10, 0.0f, ray.direction.z * 10);
+
+            playerBody.AddForce(movement * speed);
+        }
+    }
+}
